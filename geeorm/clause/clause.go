b98af25cb5193d728c50ff490a69bfc6c8d56f7e
2789459251 <18291734409@stu.xupt.edu.cn>
2024-03-18 20:30:49 +0800

    geeORM之基础框架
    (cherry picked from commit b1db02fa7d696023d3bced718b1ab5b50d09b55c)

--- geeorm/clause/clause.go
+++ geeorm/clause/clause.go
@@ -22,7 +22,7 @@ func (c *Clause) Set(name Type, vars ...interface{}) {
 		c.sql = make(map[Type]string)
 		c.sqlVars = make(map[Type][]interface{})
 	}
-	sql, vars := generators[name](vars)
+	sql, vars := generators[name](vars...)
 	c.sql[name] = sql
 	c.sqlVars[name] = vars
 }
